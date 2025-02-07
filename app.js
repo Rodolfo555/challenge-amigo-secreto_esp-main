c456613844d4c5518ccc9f476c40f01fc791a2bb
rog15 <103136472+Rodolfo555@users.noreply.github.com>
2025-02-06 21:27:09 -0600

    Variables iniciales

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Agregar Nombres A La Lista Usando Boton.
+let amigos = [];
+
+// Validar Entrada
+function agregarAmigo() {
+  let input = document.getElementById("amigo");
+}
+
+// Visualizar Lista
+
+// Sorteo Aleatorio
