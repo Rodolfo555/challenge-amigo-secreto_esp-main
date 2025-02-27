d347465b41f59a30e8b9b9534c6be1644ce6d60c
rog15 <103136472+Rodolfo555@users.noreply.github.com>
2025-02-27 03:04:01 -0600

    updated variables and funtions

--- app.js
+++ app.js
@@ -4,9 +4,44 @@ let amigos = [];
 
 // Validar Entrada
 function agregarAmigo() {
-  let input = document.getElementById("amigo");
+  let inputAmigo = document.getElementById("amigo");
+  let nombreAmigo = inputAmigo.value;
+
+  if (!nombreAmigo) {
+    alert("Debes ingresar un nombre");
+    return;
+  }
+
+  amigos.push(nombreAmigo);
+
+  inputAmigo.value = "";
+  inputAmigo.focus();
+  crearListaAmigos();
+  console.log(amigos);
 }
 
 // Visualizar Lista
+function crearListaAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+  // loop para crear la lista con los nombre a ser sorteados
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li");
+    item.textContent = amigos[i];
+    listaAmigos.appendChild(item);
+  }
+}
 
 // Sorteo Aleatorio
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("No hay amigos para sortear");
+    return;
+  }
+  let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
+  let limpiarLista = document.getElementById("listaAmigos");
+  limpiarLista.innerHTML = "";
+}
